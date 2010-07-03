c875b767fa71228ee48b2fd0c37723e71b0ffb6f
kmmbvnr <kmmbvnr@gmail.com>
2010-07-03 09:34:09 +0700

    Switch to django_hudson

---
+++ tests/test_runner.py
@@ -0,0 +1,11 @@
+PROJECT_APPS = ('django_fsm',)
+INSTALLED_APPS = ('django_hudson',) + PROJECT_APPS
+DATABASE_ENGINE = 'sqlite3'
+
+if __name__ == "__main__":
+    import sys, test_runner as settings
+    from django.core.management import execute_manager
+
+    if len(sys.argv) == 1:
+            sys.argv.append('test')
+    execute_manager(settings)
