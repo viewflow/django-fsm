bd8d8f5035497f1844553fdab40ccee358badf23
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2012-08-24 13:13:23 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Framework :: Django',
     ]
 )
