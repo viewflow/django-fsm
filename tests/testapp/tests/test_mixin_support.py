9da9fdef54649652cd1d59511299925eabca3ff2
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2015-10-14 18:43:14 +0600

    Add mixin support testcase. Close #75

---
+++ tests/testapp/tests/test_mixin_support.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.test import TestCase
+from django_fsm import FSMField, transition
+
+
+class WorkflowMixin(object):
+    @transition(field='state', source="*", target='draft')
+    def draft(self):
+        pass
+
+    @transition(field='state', source="draft", target='published')
+    def publish(self):
+        pass
+
+
+class MixinSupportTestModel(WorkflowMixin, models.Model):
+    state = FSMField(default="new")
+
+
+class Test(TestCase):
+    def test_usecase(self):
+        model = MixinSupportTestModel()
+
+        model.draft()
+        self.assertEqual(model.state, 'draft')
+
+        model.publish()
+        self.assertEqual(model.state, 'published')
