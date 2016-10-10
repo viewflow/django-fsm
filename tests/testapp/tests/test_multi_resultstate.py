530b1ba21a64761ee52b0d8688eb390bc21ecb89
Keith Bussell <kbussell@gmail.com>
2016-10-10 12:13:51 -0700

    use real next_state in post_transition signal

--- tests/testapp/tests/test_multi_resultstate.py
+++ tests/testapp/tests/test_multi_resultstate.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.test import TestCase
 from django_fsm import FSMField, transition, RETURN_VALUE, GET_STATE
+from django_fsm.signals import pre_transition, post_transition
 
 
 class MultiResultTest(models.Model):
@@ -38,3 +39,31 @@ class Test(TestCase):
         instance = MultiResultTest(state='for_moderators')
         instance.moderate(allowed=False)
         self.assertEqual(instance.state, 'rejected')
+
+
+class TestSignals(TestCase):
+    def setUp(self):
+        self.pre_transition_called = False
+        self.post_transition_called = False
+        pre_transition.connect(self.on_pre_transition, sender=MultiResultTest)
+        post_transition.connect(self.on_post_transition, sender=MultiResultTest)
+
+    def on_pre_transition(self, sender, instance, name, source, target, **kwargs):
+        self.assertEqual(instance.state, source)
+        self.pre_transition_called = True
+
+    def on_post_transition(self, sender, instance, name, source, target, **kwargs):
+        self.assertEqual(instance.state, target)
+        self.post_transition_called = True
+
+    def test_signals_called_with_get_state(self):
+        instance = MultiResultTest(state='for_moderators')
+        instance.moderate(allowed=False)
+        self.assertTrue(self.pre_transition_called)
+        self.assertTrue(self.post_transition_called)
+
+    def test_signals_called_with_return_value(self):
+        instance = MultiResultTest()
+        instance.publish(is_public=True)
+        self.assertTrue(self.pre_transition_called)
+        self.assertTrue(self.post_transition_called)
