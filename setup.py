01ef6d13a62bc500cbae58f5fb37e37d4d4d8515
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2017-03-04 10:43:05 +0600

    Fix #167

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
         "Framework :: Django :: 1.8",
         "Framework :: Django :: 1.9",
         "Framework :: Django :: 1.10",
-        "Framework :: Django :: 1.11",
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
