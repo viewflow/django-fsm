982cd61d7532365d9de56b308c7a4d8308302c15
Nicolas Delaby <nicolas@noa.one>
2017-04-03 12:04:57 +0200

    Add a test to demonstrate issue with django 1.11
    If model with state field has other fields that access their field value
    via a property or a Virtal Field, then creation of instances will fail.

---
+++ tests/testapp/tests/test_model_create_with_generic.py
@@ -0,0 +1,44 @@
+try:
+    from django.contrib.contenttypes.fields import GenericForeignKey
+except ImportError:
+    # Django 1.6
+    from django.contrib.contenttypes.generic import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.test import TestCase
+from django_fsm import FSMField, transition
+
+
+class Ticket(models.Model):
+
+    class Meta:
+        app_label = 'testapp'
+
+
+class Task(models.Model):
+    class STATE:
+        NEW = 'new'
+        DONE = 'done'
+
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    causality = GenericForeignKey('content_type', 'object_id')
+    state = FSMField(default=STATE.NEW)
+
+    @transition(field=state, source=STATE.NEW, target=STATE.DONE)
+    def do(self):
+        pass
+
+    class Meta:
+        app_label = 'testapp'
+
+
+class Test(TestCase):
+    def setUp(self):
+        self.ticket = Ticket.objects.create()
+
+    def test_model_objects_create(self):
+        """Check a model with state field can be created
+        if one of the other fields is a property or a virtual field.
+        """
+        Task.objects.create(causality=self.ticket)
