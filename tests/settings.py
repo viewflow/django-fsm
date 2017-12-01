9112fe4a9e49dc7d237f1fe0fe695f350cbbf09a
Daniel Hahler <git@thequod.de>
2017-12-01 15:13:20 +0100

    style: flake8 fixes (ignoring E501)

--- tests/settings.py
+++ tests/settings.py
@@ -1,12 +1,16 @@
 PROJECT_APPS = ('django_fsm', 'testapp',)
-INSTALLED_APPS = ('django.contrib.contenttypes', 'django.contrib.auth', 'guardian', ) + PROJECT_APPS
+INSTALLED_APPS = (
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'guardian',
+) + PROJECT_APPS
 DATABASE_ENGINE = 'sqlite3'
 SECRET_KEY = 'nokey'
 MIDDLEWARE_CLASSES = ()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        }
+    }
 }
 
 ANONYMOUS_USER_ID = 0
