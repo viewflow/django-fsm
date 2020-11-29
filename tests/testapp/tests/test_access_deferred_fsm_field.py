25562ec28b10c2487beab1d4b9e0b042ec3d9d3b
Dmytro Kyrychuk <dmytro@flowtale.ai>
2020-11-29 23:06:05 +0200

    Allow deferring FSM fields
    This commit changes the `FSMFieldMixin.get_state()` method to wrap the
    fields with Django's built-in DeferredAttribute descriptor. This
    descriptor handles fetching the field value from the database in case it
    was deferred before by using `.deferred()` or `.only()` queryset
    methods.
    
    Fixes #186

---
+++ tests/testapp/tests/test_access_deferred_fsm_field.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.test import TestCase
+from django_fsm import FSMField, transition, can_proceed
+
+
+class DeferrableModel(models.Model):
+    state = FSMField(default='new')
+
+    @transition(field=state, source='new', target='published')
+    def publish(self):
+        pass
+
+    @transition(field=state, source='+', target='removed')
+    def remove(self):
+        pass
+
+    class Meta:
+        app_label = 'testapp'
+
+
+class Test(TestCase):
+    def setUp(self):
+        DeferrableModel.objects.create()
+        self.model = DeferrableModel.objects.only('id').get()
+
+    def test_usecase(self):
+        self.assertEqual(self.model.state, 'new')
+        self.assertTrue(can_proceed(self.model.remove))
+        self.model.remove()
+
+        self.assertEqual(self.model.state, 'removed')
+        self.assertFalse(can_proceed(self.model.remove))
