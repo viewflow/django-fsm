0b0dc5686cc7159cd36520c07f1d1d6844264515
Adam Johnson <me@adamj.eu>
2022-08-25 16:15:07 +0100

    Add Django 4.0 and 4.1 PyPI classifiers

--- setup.py
+++ setup.py
@@ -35,6 +35,8 @@ setup(
         "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.1",
         "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
